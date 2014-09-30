f2c541098e15a1a1d75217451eaae13474e9203e
Prayingctrlc <codeblazer@hotmail.com>
2014-09-29 20:03:04 -0400

    initial commit

---
+++ sig.js
@@ -0,0 +1,6 @@
+document.getElementById("navbar").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='Customers.html'></a></li>" +
+"<li><a href=''></a></li>" +
+"</ul>";
